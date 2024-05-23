6b7b8298e1d7bda91364fc8d4d166915732d1981
C. Çağrı YILMAZ <cuneytcagriyilmaz@hotmail.com>
2024-05-23 18:50:15 +0300

    T1
    T1

---
+++ cagriStore/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <i className="fas fa-home"></i>
+
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+          <p className="mt-4 text-lg text-gray-700">Tailwind CSS is working correctly.</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
