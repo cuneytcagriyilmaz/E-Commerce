8adefd948586b0d35589d059b91c00899c17ebbb
C. Çağrı YILMAZ <cuneytcagriyilmaz@hotmail.com>
2024-05-27 19:20:33 +0300

    T1-Header
    T1- header completed

--- cagriStore/src/App.jsx
+++ cagriStore/src/App.jsx
@@ -1,18 +1,21 @@
-import './App.css'
 import React from 'react';
+import HomePage from './pages/HomePage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <i className="fas fa-home"></i>
-
-        <div className="text-center">
-          <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
-          <p className="mt-4 text-lg text-gray-700">Tailwind CSS is working correctly.</p>
-        </div>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          {/* <Route path="/shop" component={Shop} />
+          <Route path="/about" component={About} />
+          <Route path="/blog" component={Blog} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/pages" component={Pages} /> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ cagriStore/src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../layout/Header'
+import Navbar from '../layout/Navbar'
+
+function HomePage() {
+    return (
+        <div>
+            <div className="flex flex-col">
+
+                <Header />
+               
+            </div>
+
+        </div>
+    )
+}
+
+export default HomePage
