ad19f47f1c6452d98bb1d0c7e31b347d22c99720
C. Çağrı YILMAZ <cuneytcagriyilmaz@hotmail.com>
2024-06-18 11:52:41 +0300

    T-11  AutoLogin
    AutoLogin completed

---
+++ cagriStore/src/store/api/loginConfig.jsx
@@ -0,0 +1,27 @@
+// axiosConfig.js
+import axios from 'axios';
+
+export const instance = axios.create({
+    baseURL: 'https://workintech-fe-ecommerce.onrender.com',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+instance.interceptors.request.use((config) => {
+    console.log('Request:', config);
+    return config;
+}, (error) => {
+    console.error('Request error:', error);
+    return Promise.reject(error);
+});
+
+instance.interceptors.response.use((response) => {
+    console.log('Response:', response);
+    return response;
+}, (error) => {
+    console.error('Response error:', error);
+    return Promise.reject(error);
+});
+
+export default instance;
