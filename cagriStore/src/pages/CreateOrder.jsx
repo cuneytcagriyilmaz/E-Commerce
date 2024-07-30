819292571760b63f4f34aa2ddd512a24918fba8f
C. Çağrı YILMAZ <cuneytcagriyilmaz@hotmail.com>
2024-07-30 14:26:15 +0300

    order summary values fixed

--- cagriStore/src/pages/CreateOrder.jsx
+++ cagriStore/src/pages/CreateOrder.jsx
@@ -1,3 +1,43 @@
+// import React, { useState } from "react";
+// import AddressInfo from "../components/OrderComponents/AddressInfo";
+// import OrderInfoTypeCart from "../components/OrderComponents/OrderInfoTypeCart";
+// import CreditCardForm from "../components/OrderComponents/CreditCardForm";
+// import Header from "../layout/Header";
+// import Footer from "../components/generalComponents/Footer";
+// import OrderSummary from "../components/OrderComponents/OrderSummary";
+
+// function CreateOrder() {
+//   const [currentStep, setCurrentStep] = useState("address");
+//   const [selectedAddress, setSelectedAddress] = useState(null);
+
+//   return (
+//     <div>
+//       <Header />
+//       <div className=" px-4 py-2 w-[92%] mx-auto flex flex-col md:flex-row">
+//         <div className="flex flex-col  ">
+//           <OrderInfoTypeCart
+//             setCurrentStep={setCurrentStep}
+//             selectedAddress={selectedAddress}
+//           />
+
+//           {currentStep === "address" && (
+//             <AddressInfo setSelectedAddress={setSelectedAddress} />
+//           )}
+//           {currentStep === "payment" && <CreditCardForm />}
+//         </div>
+//         <div className="flex-row">
+//           <OrderSummary />
+//         </div>
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default CreateOrder;
+
+
+
 import React, { useState } from "react";
 import AddressInfo from "../components/OrderComponents/AddressInfo";
 import OrderInfoTypeCart from "../components/OrderComponents/OrderInfoTypeCart";
@@ -9,12 +49,13 @@ import OrderSummary from "../components/OrderComponents/OrderSummary";
 function CreateOrder() {
   const [currentStep, setCurrentStep] = useState("address");
   const [selectedAddress, setSelectedAddress] = useState(null);
+  const [grandTotal, setGrandTotal] = useState(0);
 
   return (
     <div>
       <Header />
-      <div className=" px-4 py-2 w-[92%] mx-auto flex flex-col md:flex-row">
-        <div className="flex flex-col  ">
+      <div className="px-4 py-2 w-[92%] mx-auto flex flex-col md:flex-row">
+        <div className="flex flex-col">
           <OrderInfoTypeCart
             setCurrentStep={setCurrentStep}
             selectedAddress={selectedAddress}
@@ -23,10 +64,10 @@ function CreateOrder() {
           {currentStep === "address" && (
             <AddressInfo setSelectedAddress={setSelectedAddress} />
           )}
-          {currentStep === "payment" && <CreditCardForm />}
+          {currentStep === "payment" && <CreditCardForm grandTotal={grandTotal} />}
         </div>
         <div className="flex-row">
-          <OrderSummary />
+          <OrderSummary setGrandTotal={setGrandTotal} isCreateOrderContext={true} />
         </div>
       </div>
       <Footer />
